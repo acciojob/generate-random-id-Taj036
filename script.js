753933076e8223cd4637906941a733db2e6d465a
Taj036 <121137573+Taj036@users.noreply.github.com>
2023-12-13 00:07:38 +0000

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 function makeid(l) {
   // write your code here
+	const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+	let res = '';
+	// l.forEach( ()=>{
+	for(let i = 0 ; i < l ; i++){  
+		let num = Math.floor(Math.random() * characters.length);
+		let char = characters.charAt(num);
+		res += char ;
+	}
+
+	
+	return res ;
 }
 
 // Do not change the code below.
